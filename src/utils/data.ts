32d46f5d3c175931306510dc4445723ce9a9b3d3
gustavo867 <tartatavo3@gmail.com>
2020-12-15 19:42:16 -0300

    :hammer: utils

---
+++ src/utils/data.ts
@@ -0,0 +1,132 @@
+const data = [
+  {
+    id: 1,
+    title: 'Testing Cacheee',
+    avatar_url:
+      'https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis luctus egestas urna, vitae gravida libero tempus in. Donec et condimentum ipsum. Fusce enim ex, venenatis non neque vel, ullamcorper tempor arcu. Etiam aliquam mollis commodo. Nam varius ipsum sed massa laoreet venenatis. Donec facilisis, nunc id luctus egestas, arcu tellus rhoncus dui, a gravida sem magna vitae metus. Nulla sit amet dictum velit. Maecenas dapibus interdum enim, ut faucibus metus bibendum quis. Sed ac quam neque. In eu ante ipsum. Donec sit amet arcu consequat, accumsan erat non, consequat neque. Praesent et tincidunt nisi, at facilisis est. Nulla facilisi.',
+  },
+  {
+    id: 2,
+    title: 'Testing Cacheee',
+    avatar_url:
+      'https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis luctus egestas urna, vitae gravida libero tempus in. Donec et condimentum ipsum. Fusce enim ex, venenatis non neque vel, ullamcorper tempor arcu. Etiam aliquam mollis commodo. Nam varius ipsum sed massa laoreet venenatis. Donec facilisis, nunc id luctus egestas, arcu tellus rhoncus dui, a gravida sem magna vitae metus. Nulla sit amet dictum velit. Maecenas dapibus interdum enim, ut faucibus metus bibendum quis. Sed ac quam neque. In eu ante ipsum. Donec sit amet arcu consequat, accumsan erat non, consequat neque. Praesent et tincidunt nisi, at facilisis est. Nulla facilisi.',
+  },
+  {
+    id: 3,
+    title: 'Testing Cacheee',
+    avatar_url:
+      'https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis luctus egestas urna, vitae gravida libero tempus in. Donec et condimentum ipsum. Fusce enim ex, venenatis non neque vel, ullamcorper tempor arcu. Etiam aliquam mollis commodo. Nam varius ipsum sed massa laoreet venenatis. Donec facilisis, nunc id luctus egestas, arcu tellus rhoncus dui, a gravida sem magna vitae metus. Nulla sit amet dictum velit. Maecenas dapibus interdum enim, ut faucibus metus bibendum quis. Sed ac quam neque. In eu ante ipsum. Donec sit amet arcu consequat, accumsan erat non, consequat neque. Praesent et tincidunt nisi, at facilisis est. Nulla facilisi.',
+  },
+  {
+    id: 4,
+    title: 'Testing Cacheee',
+    avatar_url:
+      'https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis luctus egestas urna, vitae gravida libero tempus in. Donec et condimentum ipsum. Fusce enim ex, venenatis non neque vel, ullamcorper tempor arcu. Etiam aliquam mollis commodo. Nam varius ipsum sed massa laoreet venenatis. Donec facilisis, nunc id luctus egestas, arcu tellus rhoncus dui, a gravida sem magna vitae metus. Nulla sit amet dictum velit. Maecenas dapibus interdum enim, ut faucibus metus bibendum quis. Sed ac quam neque. In eu ante ipsum. Donec sit amet arcu consequat, accumsan erat non, consequat neque. Praesent et tincidunt nisi, at facilisis est. Nulla facilisi.',
+  },
+  {
+    id: 5,
+    title: 'Testing Cacheee',
+    avatar_url:
+      'https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis luctus egestas urna, vitae gravida libero tempus in. Donec et condimentum ipsum. Fusce enim ex, venenatis non neque vel, ullamcorper tempor arcu. Etiam aliquam mollis commodo. Nam varius ipsum sed massa laoreet venenatis. Donec facilisis, nunc id luctus egestas, arcu tellus rhoncus dui, a gravida sem magna vitae metus. Nulla sit amet dictum velit. Maecenas dapibus interdum enim, ut faucibus metus bibendum quis. Sed ac quam neque. In eu ante ipsum. Donec sit amet arcu consequat, accumsan erat non, consequat neque. Praesent et tincidunt nisi, at facilisis est. Nulla facilisi.',
+  },
+  {
+    id: 6,
+    title: 'Testing Cacheee',
+    avatar_url:
+      'https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis luctus egestas urna, vitae gravida libero tempus in. Donec et condimentum ipsum. Fusce enim ex, venenatis non neque vel, ullamcorper tempor arcu. Etiam aliquam mollis commodo. Nam varius ipsum sed massa laoreet venenatis. Donec facilisis, nunc id luctus egestas, arcu tellus rhoncus dui, a gravida sem magna vitae metus. Nulla sit amet dictum velit. Maecenas dapibus interdum enim, ut faucibus metus bibendum quis. Sed ac quam neque. In eu ante ipsum. Donec sit amet arcu consequat, accumsan erat non, consequat neque. Praesent et tincidunt nisi, at facilisis est. Nulla facilisi.',
+  },
+  {
+    id: 7,
+    title: 'Testing Cacheee',
+    avatar_url:
+      'https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis luctus egestas urna, vitae gravida libero tempus in. Donec et condimentum ipsum. Fusce enim ex, venenatis non neque vel, ullamcorper tempor arcu. Etiam aliquam mollis commodo. Nam varius ipsum sed massa laoreet venenatis. Donec facilisis, nunc id luctus egestas, arcu tellus rhoncus dui, a gravida sem magna vitae metus. Nulla sit amet dictum velit. Maecenas dapibus interdum enim, ut faucibus metus bibendum quis. Sed ac quam neque. In eu ante ipsum. Donec sit amet arcu consequat, accumsan erat non, consequat neque. Praesent et tincidunt nisi, at facilisis est. Nulla facilisi.',
+  },
+  {
+    id: 8,
+    title: 'Testing Cacheee',
+    avatar_url:
+      'https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis luctus egestas urna, vitae gravida libero tempus in. Donec et condimentum ipsum. Fusce enim ex, venenatis non neque vel, ullamcorper tempor arcu. Etiam aliquam mollis commodo. Nam varius ipsum sed massa laoreet venenatis. Donec facilisis, nunc id luctus egestas, arcu tellus rhoncus dui, a gravida sem magna vitae metus. Nulla sit amet dictum velit. Maecenas dapibus interdum enim, ut faucibus metus bibendum quis. Sed ac quam neque. In eu ante ipsum. Donec sit amet arcu consequat, accumsan erat non, consequat neque. Praesent et tincidunt nisi, at facilisis est. Nulla facilisi.',
+  },
+  {
+    id: 9,
+    title: 'Testing Cacheee',
+    avatar_url:
+      'https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis luctus egestas urna, vitae gravida libero tempus in. Donec et condimentum ipsum. Fusce enim ex, venenatis non neque vel, ullamcorper tempor arcu. Etiam aliquam mollis commodo. Nam varius ipsum sed massa laoreet venenatis. Donec facilisis, nunc id luctus egestas, arcu tellus rhoncus dui, a gravida sem magna vitae metus. Nulla sit amet dictum velit. Maecenas dapibus interdum enim, ut faucibus metus bibendum quis. Sed ac quam neque. In eu ante ipsum. Donec sit amet arcu consequat, accumsan erat non, consequat neque. Praesent et tincidunt nisi, at facilisis est. Nulla facilisi.',
+  },
+  {
+    id: 10,
+    title: 'Testing Cacheee',
+    avatar_url:
+      'https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis luctus egestas urna, vitae gravida libero tempus in. Donec et condimentum ipsum. Fusce enim ex, venenatis non neque vel, ullamcorper tempor arcu. Etiam aliquam mollis commodo. Nam varius ipsum sed massa laoreet venenatis. Donec facilisis, nunc id luctus egestas, arcu tellus rhoncus dui, a gravida sem magna vitae metus. Nulla sit amet dictum velit. Maecenas dapibus interdum enim, ut faucibus metus bibendum quis. Sed ac quam neque. In eu ante ipsum. Donec sit amet arcu consequat, accumsan erat non, consequat neque. Praesent et tincidunt nisi, at facilisis est. Nulla facilisi.',
+  },
+  {
+    id: 11,
+    title: 'Testing Cacheee',
+    avatar_url:
+      'https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis luctus egestas urna, vitae gravida libero tempus in. Donec et condimentum ipsum. Fusce enim ex, venenatis non neque vel, ullamcorper tempor arcu. Etiam aliquam mollis commodo. Nam varius ipsum sed massa laoreet venenatis. Donec facilisis, nunc id luctus egestas, arcu tellus rhoncus dui, a gravida sem magna vitae metus. Nulla sit amet dictum velit. Maecenas dapibus interdum enim, ut faucibus metus bibendum quis. Sed ac quam neque. In eu ante ipsum. Donec sit amet arcu consequat, accumsan erat non, consequat neque. Praesent et tincidunt nisi, at facilisis est. Nulla facilisi.',
+  },
+  {
+    id: 12,
+    title: 'Testing Cacheee',
+    avatar_url:
+      'https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis luctus egestas urna, vitae gravida libero tempus in. Donec et condimentum ipsum. Fusce enim ex, venenatis non neque vel, ullamcorper tempor arcu. Etiam aliquam mollis commodo. Nam varius ipsum sed massa laoreet venenatis. Donec facilisis, nunc id luctus egestas, arcu tellus rhoncus dui, a gravida sem magna vitae metus. Nulla sit amet dictum velit. Maecenas dapibus interdum enim, ut faucibus metus bibendum quis. Sed ac quam neque. In eu ante ipsum. Donec sit amet arcu consequat, accumsan erat non, consequat neque. Praesent et tincidunt nisi, at facilisis est. Nulla facilisi.',
+  },
+  {
+    id: 13,
+    title: 'Testing Cacheee',
+    avatar_url:
+      'https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis luctus egestas urna, vitae gravida libero tempus in. Donec et condimentum ipsum. Fusce enim ex, venenatis non neque vel, ullamcorper tempor arcu. Etiam aliquam mollis commodo. Nam varius ipsum sed massa laoreet venenatis. Donec facilisis, nunc id luctus egestas, arcu tellus rhoncus dui, a gravida sem magna vitae metus. Nulla sit amet dictum velit. Maecenas dapibus interdum enim, ut faucibus metus bibendum quis. Sed ac quam neque. In eu ante ipsum. Donec sit amet arcu consequat, accumsan erat non, consequat neque. Praesent et tincidunt nisi, at facilisis est. Nulla facilisi.',
+  },
+  {
+    id: 14,
+    title: 'Testing Cacheee',
+    avatar_url:
+      'https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis luctus egestas urna, vitae gravida libero tempus in. Donec et condimentum ipsum. Fusce enim ex, venenatis non neque vel, ullamcorper tempor arcu. Etiam aliquam mollis commodo. Nam varius ipsum sed massa laoreet venenatis. Donec facilisis, nunc id luctus egestas, arcu tellus rhoncus dui, a gravida sem magna vitae metus. Nulla sit amet dictum velit. Maecenas dapibus interdum enim, ut faucibus metus bibendum quis. Sed ac quam neque. In eu ante ipsum. Donec sit amet arcu consequat, accumsan erat non, consequat neque. Praesent et tincidunt nisi, at facilisis est. Nulla facilisi.',
+  },
+  {
+    id: 15,
+    title: 'Testing Cacheee',
+    avatar_url:
+      'https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis luctus egestas urna, vitae gravida libero tempus in. Donec et condimentum ipsum. Fusce enim ex, venenatis non neque vel, ullamcorper tempor arcu. Etiam aliquam mollis commodo. Nam varius ipsum sed massa laoreet venenatis. Donec facilisis, nunc id luctus egestas, arcu tellus rhoncus dui, a gravida sem magna vitae metus. Nulla sit amet dictum velit. Maecenas dapibus interdum enim, ut faucibus metus bibendum quis. Sed ac quam neque. In eu ante ipsum. Donec sit amet arcu consequat, accumsan erat non, consequat neque. Praesent et tincidunt nisi, at facilisis est. Nulla facilisi.',
+  },
+  {
+    id: 16,
+    title: 'Testing Cacheee',
+    avatar_url:
+      'https://avatars2.githubusercontent.com/u/2254731?s=460&u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&v=4',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis luctus egestas urna, vitae gravida libero tempus in. Donec et condimentum ipsum. Fusce enim ex, venenatis non neque vel, ullamcorper tempor arcu. Etiam aliquam mollis commodo. Nam varius ipsum sed massa laoreet venenatis. Donec facilisis, nunc id luctus egestas, arcu tellus rhoncus dui, a gravida sem magna vitae metus. Nulla sit amet dictum velit. Maecenas dapibus interdum enim, ut faucibus metus bibendum quis. Sed ac quam neque. In eu ante ipsum. Donec sit amet arcu consequat, accumsan erat non, consequat neque. Praesent et tincidunt nisi, at facilisis est. Nulla facilisi.',
+  },
+];
+
+export default data;
